0f3d2c1bbbc5449ad32b501b5dbac71c4377eec9
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-03-14 11:04:45 +0100

    kapcsolat done

---
+++ src/components/pages/kapcsolat.tsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+export default class Rolunk extends Component {
+
+    handleSubmit(event: { preventDefault: () => void; }) {
+        alert('Sikeres beküldés! ');
+        event.preventDefault();
+      }
+    render() {
+        return <>
+            <form id="form" onSubmit={this.handleSubmit}>
+                <label htmlFor="email">Email:</label><br />
+                <input type="email" id="email" name="email" placeholder="example@gmail.com" required/><br />
+                <label htmlFor="message">Üzenet: </label><br />
+                <textarea name="message" form="form" id="message" placeholder="Irja ide az üzenetet..." required></textarea><br />
+                <input type="submit" value="Elküld" />
+            </form>
+           
+        </>
+    }
+}
