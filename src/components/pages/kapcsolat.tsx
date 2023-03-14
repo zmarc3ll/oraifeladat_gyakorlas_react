1af38ef8f90ab4283fdc063ee247bfd3375a785c
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-03-14 11:12:28 +0100

    div added and styled

--- src/components/pages/kapcsolat.tsx
+++ src/components/pages/kapcsolat.tsx
@@ -8,13 +8,15 @@ export default class Rolunk extends Component {
       }
     render() {
         return <>
-            <form id="form" onSubmit={this.handleSubmit}>
-                <label htmlFor="email">Email:</label><br />
-                <input type="email" id="email" name="email" placeholder="example@gmail.com" required/><br />
-                <label htmlFor="message">Üzenet: </label><br />
-                <textarea name="message" form="form" id="message" placeholder="Irja ide az üzenetet..." required></textarea><br />
-                <input type="submit" value="Elküld" />
-            </form>
+            <div className="container">
+                <form id="form" onSubmit={this.handleSubmit}>
+                    <label htmlFor="email">Email:</label><br />
+                    <input type="email" id="email" name="email" placeholder="example@gmail.com" required/><br />
+                    <label htmlFor="message">Üzenet: </label><br />
+                    <textarea name="message" form="form" id="message" placeholder="Irja ide az üzenetet..." required></textarea><br />
+                    <input type="submit" value="Elküld" />
+                </form>
+            </div>
            
         </>
     }
